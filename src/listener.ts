ed19a4474900d7be8df21a8213dd0dfebd9d4236
Phureewat A <a.phureewat@gmail.com>
2023-08-05 22:30:17 +0700

    [fix] revert listener

--- src/listener.ts
+++ src/listener.ts
@@ -1,21 +1,12 @@
 import { initMevShare } from './lib/client';
-import {
-  IPendingTransaction,
-  IPendingBundle,
-} from '@flashbots/mev-share-client';
+import { IPendingTransaction } from '@flashbots/mev-share-client';
 
 const main = async () => {
   const { mevShare } = await initMevShare();
 
   // listen for txs
   mevShare.on('transaction', async (pendingTx: IPendingTransaction) => {
-    if (
-      pendingTx.functionSelector === '0xa3c356e4' &&
-      pendingTx.callData === '0xa3c356e4'
-    ) {
-      console.info('found activeRewardSimple tx!', pendingTx.hash);
-      console.dir(pendingTx, { depth: null });
-    }
+    console.dir(pendingTx, { depth: null });
   });
 };
 
