32637f03fb1310d1c6ab1d3149672acb882c11ef
apemon <760108+apemon@users.noreply.github.com>
2023-08-05 11:57:02 +0700

    add mempool

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
-import { JsonRpcProvider, Wallet } from 'ethers';
+import { JsonRpcProvider, Wallet, WebSocketProvider } from 'ethers';
 import MevShareClient from '@flashbots/mev-share-client';
 import Env from './env';
 
@@ -7,6 +7,7 @@ export function initMevShare() {
   const authSigner = new Wallet(Env.authKey).connect(provider);
   const executorWallet = new Wallet(Env.senderKey).connect(provider);
   const mevShare = MevShareClient.useEthereumGoerli(authSigner);
+  const wssProvider = new WebSocketProvider(Env.wssProviderUrl);
 
   console.log('mev-share auth address: ' + authSigner.address);
   console.log('executor address: ' + executorWallet.address);
@@ -16,5 +17,6 @@ export function initMevShare() {
     executorWallet,
     authSigner,
     mevShare,
+    wssProvider
   };
 }
--- src/listener.ts
+++ src/listener.ts
@@ -5,7 +5,15 @@ import {
 } from '@flashbots/mev-share-client';
 
 const main = async () => {
-  const { mevShare } = await initMevShare();
+  const { mevShare, wssProvider } = await initMevShare();
+
+  // listen for mempool txs
+  wssProvider.on('pending', async (txHash: string) => {
+    console.log('mempool', txHash)
+    // Get tx data
+    const tx = await wssProvider.getTransaction(txHash);
+    // access tx for futher action
+  })
 
   // listen for txs
   mevShare.on('transaction', async (pendingTx: IPendingTransaction) => {
